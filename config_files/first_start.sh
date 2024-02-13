283c10766796bad0ef86524ffb11d37cbd82d99e
Detlef Lampart <detlef@lampart.de>
2024-02-13 18:41:11 +0100

    Corrected 'first_start.sh'

--- config_files/first_start.sh
+++ config_files/first_start.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
 mv /etc/apache2/httpd.conf /etc/apache2/httpd.conf.orig
 mv /etc/apache2/httpd.new.conf /etc/apache2/httpd.conf
-mv /etc/php81/php-fpm.conf /etc/php81/php-fpm.conf.orig
-mv /etc/php81/php-fpm.new.conf /etc/php81/php-fpm.conf
-mv /etc/php81/php-fpm.d/www.conf /etc/php81/php-fpm.d/www.conf.orig
-mv /etc/php81/php-fpm.d/www.new.conf /etc/php81/php-fpm.d/www.conf
+mv /etc/php83/php-fpm.conf /etc/php83/php-fpm.conf.orig
+mv /etc/php83/php-fpm.new.conf /etc/php83/php-fpm.conf
+mv /etc/php83/php-fpm.d/www.conf /etc/php83/php-fpm.d/www.conf.orig
+mv /etc/php83/php-fpm.d/www.new.conf /etc/php83/php-fpm.d/www.conf
 mv /root/index.html /var/www/localhost/htdocs/
 mv /root/index.php /var/www/localhost/htdocs/
 mv /root/phpinfo.php /var/www/localhost/htdocs/
@@ -14,10 +14,10 @@ chown apache:apache /var/www/localhost/htdocs/phpinfo.php
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 /sbin/php_configure.sh
 rc-update add sshd 
-rc-update add php-fpm81
+rc-update add php-fpm83
 rc-update add apache2
 rc-service sshd start
-rc-service php-fpm81 start
+rc-service php-fpm83 start
 rc-service apache2 start
 echo "****"
 echo "'"
