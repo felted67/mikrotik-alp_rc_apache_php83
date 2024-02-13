ad96437c1de50996c7fc63feaeb1a9f0527d4987
Detlef Lampart <detlef@lampart.de>
2024-02-13 17:40:02 +0100

    *** First commit

---
+++ config_files/first_start.sh
@@ -0,0 +1,31 @@
+#!/bin/ash
+mv /etc/apache2/httpd.conf /etc/apache2/httpd.conf.orig
+mv /etc/apache2/httpd.new.conf /etc/apache2/httpd.conf
+mv /etc/php81/php-fpm.conf /etc/php81/php-fpm.conf.orig
+mv /etc/php81/php-fpm.new.conf /etc/php81/php-fpm.conf
+mv /etc/php81/php-fpm.d/www.conf /etc/php81/php-fpm.d/www.conf.orig
+mv /etc/php81/php-fpm.d/www.new.conf /etc/php81/php-fpm.d/www.conf
+mv /root/index.html /var/www/localhost/htdocs/
+mv /root/index.php /var/www/localhost/htdocs/
+mv /root/phpinfo.php /var/www/localhost/htdocs/
+chown apache:apache /var/www/localhost/htdocs/index.html
+chown apache:apache /var/www/localhost/htdocs/index.php
+chown apache:apache /var/www/localhost/htdocs/phpinfo.php
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+/sbin/php_configure.sh
+rc-update add sshd 
+rc-update add php-fpm81
+rc-update add apache2
+rc-service sshd start
+rc-service php-fpm81 start
+rc-service apache2 start
+echo "****"
+echo "'"
+echo "Don't forget to set root-ssh password !!!"
+echo "*"
+echo "****"
+echo "*"
+echo "first_start.sh completed !"
+echo "*"
+echo "****"
+rc-update del auto_init
---
+++ config_files/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>PHP docker-nginx on mikrotik</title>
+</head>
+<body>
+<?php echo '<p>docker-apache with php-fpm on mikrotik</p>'; ?>
+</body>
+</html>
+
